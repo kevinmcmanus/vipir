fae820417653ca306e214541d848d33af6ecdeb3
Kevin McManus <kevin.leo.mcmanus@gmail.com>
2020-07-24 16:53:31 +0000

    ftp fetch

--- src/vipir/vipir.py
+++ src/vipir/vipir.py
@@ -1,10 +1,15 @@
 from netCDF4 import Dataset
 import numpy as np
 from datetime import datetime, timezone
-
-import matplotlib
+import datetime
+import matplotlib.pyplot as plt
 from matplotlib import cm
 from PIL import Image, ImageDraw, ImageEnhance
+
+from numpy.ma import masked_array
+import matplotlib.colors as colors
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1.colorbar import colorbar
 
 from vipir.utils import modelutils as mod_util
 
@@ -44,7 +49,7 @@ class vipir():
             
             
             # some metadata
-            self.obs_time = datetime(
+            self.obs_time = datetime.datetime(
                 np.array(vars['year'][:]).item(),
                 np.array(vars['month'][:]).item(),
                 np.array(vars['day'][:]).item(),
@@ -59,6 +64,8 @@ class vipir():
                 'latitude':np.array(vars['latitude'][:]).item(),
                 'altitude':np.array(vars['altitude'][:]).item()
             }
+            
+            self.colormaps = None
 
     # TODO: figure out a way to encapsulate the snr computation
     def snr(self, which='total_power', thresh=3):
@@ -72,6 +79,7 @@ class vipir():
             pwr = self.X_mode_power
             noise = self.X_mode_noise
         elif which == 'O_mode_power_adj':
+            raise ValueError('O_mode_power_adj no longer supported')
             X_mode_snr = self.X_mode_power - self.X_mode_noise.reshape(-1,1)
             O_mode_snr = self.O_mode_power - self.O_mode_noise.reshape(-1,1)
             pwr = np.where(X_mode_snr > O_mode_snr + thresh, X_mode_snr, O_mode_snr)
@@ -84,14 +92,36 @@ class vipir():
             raise ValueError(s)
             
         return pwr-noise.reshape(-1,1)
+    
+    def img_array(self, thresh=3):
+        cmaps = self.get_colormaps()
+        norm = colors.Normalize(vmin=0, vmax=100)
+        
+        #get the power measures
+        o_pwr = self.snr(which='O_mode_power')
+        x_pwr = self.snr(which='X_mode_power')
+        
+        # identify the cells in which x_pwr exceeds o_pwr by more than the threshold
+        mask = x_pwr > o_pwr+thresh
+        
+        # mask out the o_ and x_pwr values
+        x_pwr = x_pwr*mask
+        o_pwr = o_pwr*np.logical_not(mask)
 
-    def image(self, which='total_power',size=(1024, 1024), enhance=True):
+        o_np = cmaps['red_hsv'](norm(o_pwr),bytes=True)
+        x_np = cmaps['green_hsv'](norm(x_pwr),bytes=True)
+        
+        #put them together and transpose
+        img_np = np.transpose(o_np + x_np, axes=[1,0,2])
+        
+        return img_np
 
-        snr = self.snr(which)
-        cmap = matplotlib.cm.get_cmap('gnuplot')
-        norm = matplotlib.colors.Normalize(vmin=0, vmax=100)
-        snr_np = cmap(norm(snr.T), bytes=True)
-        snr_im = Image.fromarray(snr_np, mode='RGBA').transpose(Image.FLIP_TOP_BOTTOM)
+    def image(self, which='total_power',size=(1024, 1024), thresh=3, enhance=True):
+        
+        img_np = self.img_array(thresh=thresh)
+        
+        # build the image in the proper orientation
+        snr_im = Image.fromarray(img_np, mode='RGBA').transpose(Image.FLIP_TOP_BOTTOM)
 
         # Jack the contrast if asked
         if enhance:
@@ -104,6 +134,34 @@ class vipir():
         im = im.resize(size)
         self.imsize = im.size
         return im.convert('RGB')
+    
+    def plot_obs(self, ax=None):
+        
+        cmaps = self.get_colormaps()
+        norm = matplotlib.colors.Normalize(vmin=0, vmax=100)
+        
+        #get the plot axes
+        freq = self.freq
+        rng = self.rng
+        
+        #get the power measures
+        o_pwr = self.snr(which='O_mode_power')
+        x_pwr = self.snr(which='X_mode_power')
+        
+        # identify the cells in which x_pwr exceeds o_pwr by more than the threshold
+        mask = x_pwr > o_pwr+thresh
+        
+        # mask out the o_ and x_pwr values
+        x_pwr_m = masked_array(x_pwr, np.logical_not(mask))
+        o_pwr_m = masked_array(o_pwr, mask)
+        
+        px = ax.pcolormesh(x_pwr_m.T,cmap=green_cmap,
+               norm=colors.PowerNorm(gamma=gamma),
+              vmin=0, vmax=50)
+        
+        po = ax.pcolormesh(o_pwr_m.T,cmap=red_cmap,
+               norm=colors.PowerNorm(gamma=gamma),
+              vmin=0, vmax=50)
 
     def get_objects(self, model, thresh=0.7):
         mod = model['model']
@@ -186,3 +244,239 @@ class vipir():
                 self.rng[rngfreq_i['minrng_i']:rngfreq_i['maxrng_i']])
 
 
+    def get_colormaps(self):
+        if self.colormaps is None:
+            self.set_colormaps(self.gen_colormaps())
+        return self.colormaps
+    
+    def set_colormaps(self, cmaps):
+        self.colormaps = cmaps
+        
+    def gen_colormaps(self):
+        """
+        generates rbga colormaps equivalents for red and green HSV cmaps
+        """
+        from matplotlib.colors import ListedColormap
+        
+        #this should probably be somewhere else!
+        #references: Equations: (https://www.rapidtables.com/convert/color/hsv-to-rgb.html)
+        #            Good Article: (http://colorizer.org/)
+
+        def hsv_to_rgb(h,s,v):
+            """
+            converts hue-saturation-value (hsv) to rgb (rgba, actually)
+            """
+            c = v*s
+            x = c*(1-np.abs(((h/60.0) % 2)-1))
+            m = v-c
+
+            rgb_tbl = np.array([[c,x,0],
+                              [x,c,0],
+                              [0,c,x],
+                              [0,x,c],
+                              [x,0,c],
+                              [c,0,x]])
+
+            rgb_prime =  rgb_tbl[int(h//60.0)]
+
+            rgb = (rgb_prime+m)  #*255
+
+            rgba = np.array([*rgb,1])
+
+            return rgba
+        
+        N = 256
+        sv = np.linspace(0.0, 1.0, N)
+        
+        # red is 0.0 degree hue, green is 90.0 degree hue in hsv scheme
+        # use 1.0 for sat value, color map walks up the outer edge of the cone
+        red_hsv   = np.array([hsv_to_rgb( 0.0, 1.0, v)for v in sv])
+        green_hsv = np.array([hsv_to_rgb(90.0, 1.0, v)for v in sv])
+        
+        #make the color maps
+        red_hsv_cmap   = ListedColormap(red_hsv)
+        green_hsv_cmap = ListedColormap(green_hsv)
+        
+        return {'red_hsv':red_hsv_cmap, "green_hsv":green_hsv_cmap}
+
+
+    def plot_pwr(self, fig, thresh=3.0, gamma=0.5):
+
+        o_pwr = self.snr('O_mode_power')
+        x_pwr = self.snr('X_mode_power')
+        x_pwr_m = masked_array(x_pwr, x_pwr <=  o_pwr+thresh)
+        o_pwr_m = masked_array(o_pwr, x_pwr >   o_pwr+thresh)
+
+        freq = self.freq
+        rng = self.rng
+
+        #set up the figure
+        gs = gridspec.GridSpec(1,3, width_ratios=[8,1,1])
+        ax = fig.add_subplot(gs[0], frameon=True)
+
+
+        cmaps = self.get_colormaps()
+
+        px = ax.pcolormesh(freq, rng, x_pwr_m.T,cmap=cmaps['green_hsv'],
+                   norm=colors.PowerNorm(gamma=gamma),
+                  vmin=0, vmax=50)
+        cax_x = fig.add_subplot(gs[1],frameon=False)
+        cba = plt.colorbar(px, cax=cax_x, shrink=0.75, fraction=0.5,
+                           extend='max')
+        cax_x.set_title('X-Power', pad=0.1)
+
+        po = ax.pcolormesh(freq, rng, o_pwr_m.T,cmap=cmaps['red_hsv'],
+                       norm=colors.PowerNorm(gamma=gamma),
+                      vmin=0, vmax=50)
+        cax_o = fig.add_subplot(gs[2],frameon=False)
+        cbb = plt.colorbar(po, cax=cax_o,shrink=0.25, fraction=0.5,
+                         extend='max')
+        cax_o.set_title('O-Power')
+        cax_o.set_ylabel('SNR (dB)', rotation=270, labelpad=0.15)
+
+        ax.set_xscale('log')
+        #ax.set_yscale('log')
+
+        ax.set_ylabel('Range (km)')
+        ax.set_xlabel('Frequency log(kHz)')
+
+        ax.set_title(self.station + ' ' +self.obs_time.strftime('%Y-%m-%d %H:%M:%S %Z'))
+
+        return ax
+    
+
+    def get_trace_bbox(self, imthresh = 30.0, thresh=3.0, size=5):
+        import scipy.ndimage as nd
+        
+        #make the image power array
+        x_pwr = self.snr('X_mode_power')
+        o_pwr = self.snr('O_mode_power')
+        pwr = np.where(x_pwr >= o_pwr+thresh, x_pwr, o_pwr)
+
+        #convolve (max) to increase the connectivity
+        pwr = nd.generic_filter(pwr,np.max,size=size,mode='nearest')
+
+        #mask out the backgound (and transpose)
+        im = pwr.T >= imthresh
+
+        #label and find the features in the masked image
+        feats, nfeats = nd.label(im, structure=nd.generate_binary_structure(2,2))
+        #get the object slices
+        objs = nd.find_objects(feats)
+
+        #calculate the areas of the objects and find the largest
+        areas = np.array([ (s[0].stop-s[0].start)*(s[1].stop-s[1].start) for s in objs])
+        b = areas.argmax() #index of the object covering largest area
+
+        #dredge up the freq and range vals for the bbox
+        bbox = {'freqstart': self.freq[objs[b][1].start],
+                'freqend':   self.freq[objs[b][1].stop-1],
+                'rngstart':  self.rng[ objs[b][0].start],
+                'rngend':    self.rng[ objs[b][0].stop-1]}
+
+        return bbox
+
+import tempfile
+from ftplib import FTP
+import os
+
+def get_cdf(stn=None,dtstr=None, path=None, ftpsite ='ftp.ngdc.noaa.gov', rootdir='/ionosonde/data'):
+    """
+    Returns a vip for a station and date-time string
+    """
+    
+    if path is None:
+        yr = dtstr[0:4]
+        daynum=dtstr[4:7]
+
+        cdfpath = f'{rootdir}/{stn}/individual/{yr}/{daynum}/ionogram/{stn}_{dtstr}.NGI'
+    else:
+        cdfpath = path
+        
+    # create a temp file to fetch the cdf into
+    fh =  tempfile.mkstemp()
+
+    #fetch the cdf
+    with open(fh[1],'wb') as fout:
+        with FTP(ftpsite) as ftp:
+            ftp.login()
+            ftp.retrbinary(f'RETR {cdfpath}', fout.write)
+            
+    # read up as a vip what we just fetched        
+    vip = vipir(fh[1])
+    
+    #ditch the temp file
+    os.remove(fh[1])
+    
+    return vip
+
+from datetime import datetime, timezone, timedelta
+def obsname_todt(obsname:str) -> datetime:
+    year = int(obsname[6:10])
+    daynum = int(obsname[10:13]) # day of the year
+    hr = int(obsname[13:15])
+    min = int(obsname[15:17])
+    sec = int(obsname[17:19])
+    
+    dt = datetime(year, 1,1, hr, min, sec,tzinfo=timezone.utc )+timedelta(daynum-1)
+    
+    return dt
+
+
+
+def get_flist(stn, fromstr, tostr, interval = timedelta(minutes=10),
+              ftpsite ='ftp.ngdc.noaa.gov',rootdir='/ionosonde/data'):
+    """
+    fromstr and tostr are utc in iso format
+    """
+    
+    #convert fromstr and tostr to UTC  datetime objects
+    fromdt = datetime.fromisoformat(fromstr)
+    fromdt = fromdt.replace(tzinfo=timezone.utc)  
+    todt = datetime.fromisoformat(tostr)
+    todt = todt.replace(tzinfo=timezone.utc)
+
+    #set up ftp connection
+    ftp = FTP(ftpsite)
+    ftp.login()
+    
+    #loop over the days and accumulate the qualifying file names
+    lastdt = thisdt = fromdt
+    flist = []
+    try:
+        while thisdt <= todt:
+
+            #change to the data directory for the current day
+            yr = thisdt.year
+            daynum =  thisdt.timetuple().tm_yday
+            dir = f'{rootdir}/{stn}/individual/{yr}/{daynum:03}/ionogram/'
+            ftp.cwd(dir)
+
+            #get the NGI files and extract their date time from their names
+            obs_list = [f for f in ftp.nlst() if re.search(r'^.*.NGI$',f)]
+            obs_list.sort() # make sure in lexo/chrono order
+            # get the datetimes from the file names
+            obs_dt = [obsname_todt(o) for o in obs_list]
+
+            #filter the file name list to be between fromdt and todt
+            obs_all = [(f,o) for f,o in zip(obs_list,obs_dt) if o >= fromdt and o <= todt]
+            
+            #space out by the interval
+            #find the next file that is at least 'interval' later than its predecessor
+            obs_list = []
+            for oa in obs_all:
+                if (oa[1]-lastdt) >= interval:
+                    obs_list.append((oa[1].strftime('%Y-%m-%d %H:%M:%S'), dir + oa[0]))
+                    lastdt = oa[1]
+
+            #accumulate the filtered file names
+            flist += obs_list
+
+            #see what tomorrow brings
+            thisdt += timedelta(days=1)
+    
+    finally:
+        ftp.close()
+    
+    return flist
+        
