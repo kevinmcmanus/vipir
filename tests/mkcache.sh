08203fc1082026211135778e1f3bcd7bcb48b00c
Kevin McManus <kevin.leo.mcmanus@gmail.com>
2023-05-31 17:59:19 -0600

    Handoff includes some test scripts to ensure all installs  correctly

---
+++ tests/mkcache.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+#conda activate vipir
+
+python ../src/mkcache.py WI937 2021-09-05T00:00:00 2021-09-05T23:59:59 ../data/20210905_WI937 --interval 60
---
+++ tests/run_animation.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+#conda activate vipir
+
+python ../src/pwr_animate.py --dir ../data/20210905_WI937
